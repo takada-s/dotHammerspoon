38255c6d1cfcdce878861ed1e0eb6333782c600a
TAKADA Shingo <takada_shingo@cyberdyne.jp>
2017-02-01 11:30:18 +0900

    key mapping is now moved to config

--- config.lua
+++ config.lua
@@ -2,3 +2,39 @@
 BROWSER1 = 'Firefox'
 BROWSER2 = 'Google Chrome'
 BROWSER3 = 'Safari'
+
+EIKANA_MAPPING = {
+    kana = {
+        -- single letter key: launcher
+        a = { activateApp = 'Activity Monitor' },
+        b = { activateApp = 'Mail' },
+        e = { activateApp = 'Emacs' },
+        f = { activateApp = BROWSER1 },
+        g = { activateApp = BROWSER2 },
+        h = { activateApp = BROWSER3 },
+        i = { activateApp = 'iTunes' },
+        q = { activateApp = 'OmniFocus' },
+        r = { activateApp = 'RubyMine' },
+        s = { activateApp = 'Calendar' },
+        t = { activateApp = 'iTerm' },
+        v = { activateApp = 'Messages' },
+        w = { activateApp = 'Wunderlist' },
+        -- PgUp/Dn: volume control
+        pageup = { replaceToSystemKey = 'SOUND_UP' },
+        pagedown = { replaceToSystemKey = 'SOUND_DOWN' },
+        -- PrintSc/ScLock/Pause (f13,f14,f15): audio control
+        f13 = { replaceToSystemKey = 'PREVIOUS' },
+        f14 = { replaceToSystemKey = 'PLAY' },
+        f15 = { replaceToSystemKey = 'NEXT' },
+    },
+    eisu = {
+        -- hjkl: move cursor
+        h = { replaceToKey = 'left' },
+        j = { replaceToKey = 'down' },
+        k = { replaceToKey = 'up' },
+        l = { replaceToKey = 'right' },
+        -- a,e: home / end
+        a = { replaceToKey = 'home' },
+        e = { replaceToKey = 'end' },
+    }
+}
